f49440f7182af438b1b6f26e4bab206a6241fb1a
juanreina <juandavidreina.ms@gmail.com>
2025-05-31 17:44:25 -0500

    feat: class separation

---
+++ with-solid.js
@@ -0,0 +1,12 @@
+// Interfaces 
+class OrderSaver {
+    save(orderData) {
+        throw new Error("Not implemented");
+    }
+}
+
+class Notifier {
+    send(message) {
+        throw new Error("Not implemented");
+    }
+}
