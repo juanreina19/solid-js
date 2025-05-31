e55e6f70fe08a35f2c509e5d063c4138eae218bf
juanreina <juandavidreina.ms@gmail.com>
2025-05-31 17:47:12 -0500

    feat: class OrderProccesor & object creation

--- with-solid.js
+++ with-solid.js
@@ -32,4 +32,43 @@ class SMSNotifier extends Notifier {
     }
 }
 
+// Clase para procesar ordenes
+class OrderProcessor {
+    constructor(orderData, saver, notifier) {
+        this.orderData = orderData;
+        this.saver = saver;
+        this.notifier = notifier;
+    }
 
+    validOrder() {
+        if (!this.orderData.items || this.orderData.items.length === 0) {
+            throw new Error('Order must have at least one item');
+        }
+    }
+
+    calculateTotal() {
+        return this.orderData.items.reduce((sum, item) => sum + item.price * item.quantity, 0);
+    }
+
+    process() {
+        this.validOrder();
+        const total = this.calculateTotal();
+        this.saver.save(this.orderData);
+        this.notifier.send(`${orderData.customerEmail} Thank you for your purcharse.`);
+        return total;
+    }
+}
+
+const orderData = {
+    customerEmail: 'user@example.com',
+    items: [
+        { name: 'Laptop', price: 1200, quantity: 1 },
+        { name: 'Mouse', price: 25, quantity: 2 }
+    ]
+};
+
+const saver = new FileOrderSaver();
+const notifier = new EmailNotifier();
+
+const processor = new OrderProcessor(orderData, saver, notifier);
+console.log('Total: $', processor.process());
