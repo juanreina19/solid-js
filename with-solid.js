db3ce502c7d9729e8ae8e0399eddbc9623a49f74
juanreina <juandavidreina.ms@gmail.com>
2025-05-31 17:45:54 -0500

    feat: class extender

--- with-solid.js
+++ with-solid.js
@@ -10,3 +10,26 @@ class Notifier {
         throw new Error("Not implemented");
     }
 }
+
+// Implementación de OrderSaver
+class FileOrderSaver extends OrderSaver {
+    save(orderData) {
+        const fs = require('fs');
+        fs.writeFileSync('order.json', JSON.stringify(orderData));
+    }
+}
+
+// Implementación de Notifier
+class EmailNotifier extends Notifier {
+    send(message) {
+        console.log(`Sending email: ${message}`);
+    }
+}
+
+class SMSNotifier extends Notifier {
+    send(message) {
+        console.log(`Sending SMS: ${message}`);
+    }
+}
+
+
